88978ff51ba5e4c014fc4a7bdf1e6aef39fda2c1
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-03-18 21:39:33 +0300

    edited.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
-import MobilHeader from "@/components/MobilHeader";
-import Sidebar from "@/components/Sidebar";
+import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/mobile-header";
 
 type Props = {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <MobilHeader />
+      <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
